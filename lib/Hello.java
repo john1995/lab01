89ba4bb294ee1b8b125c9cb95e25b2d0fff3c338
Rene <risanche@ucsd.edu>
2014-10-08 16:43:07 -0700

    Made interactive

--- lib/Hello.java
+++ lib/Hello.java
@@ -2,14 +2,10 @@ public class Hello
 {
 	public static void main (String argv[] )
 	{
-	    // Default is "World"
-	    //Author:Jim Weirich (jim@somewhere.com)
-	    String name = "World";
-	    if (argv.length != 0) 
-		{
-		    name = argv[0];
-		}
+	    System.out.println("What's your name");
+        Scanner in = new Scanner(System.in);
+        String my_name = in.nextLine();
 
-		System.out.println( "Hello, " + name  + "!" );
+        System.out.println( "Hello, " + my_name + "!" );
 	}
 }
