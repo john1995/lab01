1ccd06d019ee60425d5e3773e153fc5b37a3a373
Rene <risanche@ucsd.edu>
2014-10-08 15:34:08 -0700

    Added a comment

--- Hello.java
+++ Hello.java
@@ -2,12 +2,13 @@ public class Hello
 {
 	public static void main (String argv[] )
 	{
+	    // Default is "World"
 	    String name = "World";
 	    if (argv.length != 0) 
 		{
 		    name = argv[0];
 		}
 
-		System.out.println( "Hello, " + argv[0] + "!" );
+		System.out.println( "Hello, " + name  + "!" );
 	}
 }
